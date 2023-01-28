f58474e54cfa91f7bd981d1467d8cdec0d471bc2
tony tolver <andrademaresantonio00@gmail.com>
2023-01-28 14:46:47 -0600

    Creación de formatos csv, json, xlsx

--- BeautifulS/pts_ligamx.py
+++ BeautifulS/pts_ligamx.py
@@ -1,6 +1,6 @@
 from bs4 import BeautifulSoup
 import requests
-import pandas as pd
+import pandas
 
 url = 'https://mexico.as.com/resultados/futbol/mexico_clausura/clasificacion/?omnil=mpal'
 page = requests.get(url) # crear la solicitud
@@ -26,7 +26,7 @@ for i in eq:
 
 # print(equipos, len(equipos))  # test para ver lso equipos extraidos en consola
 
-print("-"*50)
+print("-"*50,"Documentos creados")
 
 # Puntos
 pt = soup.find_all('td', class_='destacado')   # colocar el nombre de la clase que se quiere extraer, destacado
@@ -45,11 +45,13 @@ for i in pt:
       break
 
 # uso de pandas para crear un dataframe
-df = pd.DataFrame({'Equipos': equipos, 'Puntos': puntos},  index = list(range(1,19)))
+df = pandas.DataFrame({'Equipo': equipos, 'Puntos': puntos}, index=list(range(1,19)))
 
-# print(df) test para ver el dataframe en consola
+# save in csv
+df.to_csv('pts_ligamx.csv', index=False, encoding='utf-8')
 
-## guardar el dataframe en un archivo excel en la misma carpeta del script
-df.to_excel('pts_liga_mx.xlsx', sheet_name='pts_liga_mx', index=False)
+# # save in excel
+df.to_excel('pts_ligamx.xlsx', index=False)
 
-
+# save in json
+df.to_json('pts_ligamx.json', orient='records')
